44a1bf955a7a04012dac3d9bf693370d2d1d4df5
hippocampercams <hippocampercams@gmail.com>
2026-03-18 16:25:42 +0800

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,49 @@
+window.HPC_POSTS = [
+  {
+    id: "fujifilm-finepix-a500",
+    type: "photo",
+    meta: "Posted 2 hours ago • 204 notes",
+    titleHtml: '<p><b>FUJIFILM FINEPIX A500</b> $170.00 </p><p><i>striking, clean, elegant, moody</i></p>',
+    captionHtml: '5 mp・3x optical zoom + digital zoom・xd card・AA batteries (one less charger!)',
+    tags: ["cleangirl", "simpleelegance"],
+    images: [
+      {
+        src: "https://64.media.tumblr.com/f17a5438562dea5df5734047203241a2/2527f28ed92627d9-75/s2048x3072/9302a1564a76591a5b2f76651152f9772eb2b816.jpg",
+        alt: "FUJIFILM FINEPIX A500 photo 1"
+      },
+      {
+        src: "https://64.media.tumblr.com/fed7e8350733b5445ebfec37d2a3f7b4/b697605a47f236c2-29/s2048x3072/a47dd832a56d5848ee0c471f597e6128183735f3.jpg",
+        alt: "FUJIFILM FINEPIX A500 photo 2"
+      },
+      {
+        src: "https://64.media.tumblr.com/ff3ecf24923684b1e6f780fa3fe841e4/8f15677795d9ae9a-11/s2048x3072/15d7badcd307a9ecf554c3bcbfb84d8bd7fc00b1.jpg",
+        alt: "FUJIFILM FINEPIX A500 photo 3"
+      },
+      {
+        src: "https://64.media.tumblr.com/40f3948775a3f9f49ed6c03ba3616698/8f15677795d9ae9a-ad/s2048x3072/7dbdea2d0b465cdda97b0e8076e6135181540d2a.jpg",
+        alt: "FUJIFILM FINEPIX A500 photo 4"
+      },
+      {
+        src: "https://64.media.tumblr.com/a061ef0d07620c74b6d22b85329e0435/8f15677795d9ae9a-60/s2048x3072/3e7e37696f898aba7065a38bba2bb1a49cae74b2.jpg",
+        alt: "FUJIFILM FINEPIX A500 photo 5"
+      },
+      {
+        src: "https://64.media.tumblr.com/5627429eeaa8fb7217de976992844470/2527f28ed92627d9-f2/s2048x3072/1e03617f200b83da6ba66c3669ce27b09aa7b0ef.jpg",
+        alt: "FUJIFILM FINEPIX A500 photo 6"
+      }
+    ],
+    conditionSummaryHtml: "<p>item condition: ⋆⋆⋆⋆ (pretty good!)</p>",
+    conditionDetailsHtml: `
+      <p>this easy-to-use point and shoot cam functions normally and is in great condition for its age. there are scratches at the shutter and the back on the right, small dents at the top and sides, and minor scratches and scuffs consistent with a used vintage item.</p>
+      <div class="photo-grid">
+        <img src="INSERT PHOTO" class="full_image" alt="FUJIFILM FINEPIX A500 condition 1" onclick="openHpcLightbox(this)">
+        <img src="INSERT PHOTO" class="full_image" alt="FUJIFILM FINEPIX A500 condition 2" onclick="openHpcLightbox(this)">
+        <img src="INSERT PHOTO" class="full_image" alt="FUJIFILM FINEPIX A500 condition 3" onclick="openHpcLightbox(this)">
+        <img src="INSERT PHOTO" class="full_image" alt="FUJIFILM FINEPIX A500 condition 4" onclick="openHpcLightbox(this)">
+        <img src="INSERT PHOTO" class="full_image" alt="FUJIFILM FINEPIX A500 condition 5" onclick="openHpcLightbox(this)">
+        <img src="INSERT PHOTO" class="full_image" alt="FUJIFILM FINEPIX A500 condition 6" onclick="openHpcLightbox(this)">
+      </div>
+    `,
+    adoptUrl: "adoptacam.html?newbaby=FUJIFILM%20FINEPIX%20A500"
+  }
+];
